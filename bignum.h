015e99904d8efd735e57fd0edf0c9da7bfe7e455
joel <joel_11s@hotmail.com>
2021-05-23 20:26:10 -0300

    Añadiendo archivos

---
+++ bignum.h
@@ -0,0 +1,17 @@
+#ifndef _BIGNUM_
+#define _BIGNUM_
+
+class bignum {
+private:
+    unsigned short *digits;
+    // ...
+public:
+    // ...
+    friend bignum operator+(const bignum&, const bignum&);
+    friend bignum operator-(const bignum&, const bignum&);
+    friend bignum operator*(const bignum&, const bignum&);
+    friend std::ostream& operator<<(std::ostream&, const bignum&);
+    friend std::istream& operator>>(std::istream&, bignum&);
+};
+
+#endif
